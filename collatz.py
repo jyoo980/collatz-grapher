93c2dd3e450b1ddafebebbde8a520601bbedb223
James Yoo <jyoo@appneta.com>
2017-10-31 16:13:26 -0700

    Fixed indenting

--- collatz.py
+++ collatz.py
@@ -1,23 +1,34 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 #   collatz.py
 #   James Yoo
 #   Created: October 31, 2017
 
 def collatz(n):
-  steps = 0
-  while n > 0:
-    if n == 1:
-      return steps
-    elif n % 2 == 0:
-      n = n / 2
-      steps += 1
-    elif n % 2 == 1:
-      n = (3 * n) + 1
-      steps += 1
-    else:
-      print("Error\n")
+    steps = 0
+    while steps > 0:
+      if n == 1:
+        return steps
+      elif n % 2 == 0:
+        n = n / 2
+        steps += 1
+      elif n % 2 == 1:
+        n = (n * 3) + 1
+        steps += 1
 
+    return steps
 
-def data_generator(n):
-  input_x = range(1, n + 1)
-  output_y = [collatz(num) for num in input_x]
- 
+def graph_collatz(n):
+    input_x = range(1, 1000000 + 1)
+    output_y = [collatz(num) for num in input_x]
+  
+    trace = go.Scatter(
+      x = input_x,
+      y = output_y,
+      mode = 'markers'
+    )
+
+    data = [trace]
+    py.iplot(data, filename='collatz-trial')
+
