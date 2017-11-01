88d41f69725cf85d1c74c515df7dff30020bbc49
James Yoo <jyoo980@gmail.com>
2017-10-31 19:30:10 -0700

    Finished graph logic

--- collatz.py
+++ collatz.py
@@ -1,34 +1,33 @@
-import plotly.plotly as py
-import plotly.graph_objs as go
+import matplotlib.pyplot as plt
 
 #   collatz.py
 #   James Yoo
 #   Created: October 31, 2017
 
+
 def collatz(n):
     steps = 0
-    while steps > 0:
-      if n == 1:
-        return steps
-      elif n % 2 == 0:
-        n = n / 2
-        steps += 1
-      elif n % 2 == 1:
-        n = (n * 3) + 1
-        steps += 1
+    while n > 0:
+        if n == 1:
+            return steps
+        elif n % 2 == 0:
+            n = n / 2
+            steps += 1
+        elif n % 2 == 1:
+            n = (n * 3) + 1
+            steps += 1
 
     return steps
 
-def graph_collatz(n):
-    input_x = range(1, 1000000 + 1)
-    output_y = [collatz(num) for num in input_x]
-  
-    trace = go.Scatter(
-      x = input_x,
-      y = output_y,
-      mode = 'markers'
-    )
 
-    data = [trace]
-    py.iplot(data, filename='collatz-trial')
+def generate_data(n):
+    x_data = range(1, n + 1)
+    y_data = [collatz(n) for n in x_data]
+    plt.scatter(x_data, y_data, s=0.5)
+    plt.show()
+
+
+upper = input("Enter the number you'd like to go up to: ")
+generate_data(int(upper))
+
 
