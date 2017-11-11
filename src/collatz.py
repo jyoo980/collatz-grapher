25c501075dd78beb2b8580aafc4383dfd363480a
James Yoo <jyoo980@gmail.com>
2017-11-11 15:22:06 -0800

    Added path functionality

--- src/collatz.py
+++ src/collatz.py
@@ -21,7 +21,21 @@ def collatz(n):
     return steps
 
 
-def generate_data(n):
+def collatz_path(n):
+    path = []
+    while n > 0:
+        path.append(n)
+        if n == 1:
+            return path
+        elif n % 2 == 0:
+            n = n / 2
+        else:
+            n = (n * 3) + 1
+    
+    return path
+
+
+def generate_steps(n):
     x_data = range(1, n + 1)
     p = Pool(10)
     y_data = p.map(collatz, x_data) 
@@ -32,7 +46,21 @@ def generate_data(n):
     plt.show()
 
 
+def generate_path(n):
+    x_data = range(1, n + 1)
+    p = Pool(10)
+    y_data = p.map(collatz_path, x_data)
+    
+    for x_d, y_d in zip(x_data, y_data):
+        plt.scatter([x_d] * len(y_d), y_d)
+
+    plt.title("Collatz Path for Integers up to " + str(n))
+    plt.xlabel("Input value")
+    plt.ylabel("Path", rotation=0)
+    plt.show()
+
+
 upper = input("Enter the number you'd like to go up to: ")
-generate_data(int(upper))
+generate_path(int(upper))
 
 
